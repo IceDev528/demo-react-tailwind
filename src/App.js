283219d65c513242792165e91737856a726a38da
IceDev528 <riverforest528@outlook.com>
2021-08-31 06:34:27 +0300

    react tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" className="p-6 items-center justify-center">
+        <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
+        <p className="tracking-widest">This is my first React App.</p>
+    </div>
+  );
+}
+
+export default App;
